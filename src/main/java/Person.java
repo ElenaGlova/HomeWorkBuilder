16d3d5d7bcb7d7beccaa65c73832954f80a3c6e0
ElenaGolovataya <golovataya.e@yandex.ru>
2022-10-14 12:51:30 +0300

    First commit

---
+++ src/main/java/Person.java
@@ -0,0 +1,60 @@
+public class Person {
+    private final String name;
+    private final String surname;
+    private int age = -1;
+    private String city;
+
+    public Person(String name, String surname, int age, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.city = city;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setAge(0)
+                .setCity(city)
+                .setSurname(surname);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return name + " по фамилии " + surname;
+    }
+}
